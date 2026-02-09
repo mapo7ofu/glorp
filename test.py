1b2b7884774f09953dd60dbb6ff731fe390c7f4d
mapo7ofu <d.q.dufour@gmail.com>
2026-02-09 17:08:43 +0100

    added big list

---
+++ test.py
@@ -0,0 +1,37 @@
+
+from flask import Flask, render_template
+import csv
+
+app = Flask(__name__)
+data = []
+shown_types = ['naam', 'code']
+
+with open('art.csv', 'r') as csv_file:
+	csv_reader = csv.DictReader(csv_file)
+
+	with open('art_new.csv', 'w') as new_file:
+		fieldnames = list(next(csv_reader).keys())
+		csv_writer = csv.DictWriter(new_file, delimiter='\t', fieldnames=fieldnames)
+
+		csv_writer.writeheader()
+
+		for line in csv_reader:
+			csv_writer.writerow(line)
+
+with open('art_new.csv', 'r') as csv_file:
+	csv_reader = csv.DictReader(csv_file, delimiter='\t')
+
+	for dictionary in csv_reader:
+		data.append(dictionary)
+
+@app.route('/')
+
+def index():
+	return render_template('index.html', data=data, fieldnames=fieldnames, shown_types=shown_types)
+
+@app.route('/user/<name>')
+
+def user(name):
+        return render_template('user.html', username=name)
+
+app.run(host="0.0.0.0", port=5000)
